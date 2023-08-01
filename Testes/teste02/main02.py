d8f3fd13dc1b67d68a601085529769338a15b445
João Vitor Nishimoto Bonfim <137560778+joaovitornishimoto@users.noreply.github.com>
2023-08-01 10:00:38 -0300

    criei main02.

---
+++ Testes/teste02/main02.py
@@ -0,0 +1,5 @@
+nome = str(input("Qual é o seu nome: "))
+idade = str(input("Qual é a sua idade: "))
+peso = str(input("Quanto você pesa: "))
+print("Confirma pra mim, seu nome é {}, você tem {} anos de idade e pesa {}kg, correto?!".format(nome, idade, peso))
+
